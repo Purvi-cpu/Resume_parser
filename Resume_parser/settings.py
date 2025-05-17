b9f2e7c0c29e638116c96fb96ea5379dcac1b299
Purvi-cpu <purvisharma.ps45@gmail.com>
2025-05-17 22:25:19 +0530

    Update settings.py

--- Resume_parser/settings.py
+++ Resume_parser/settings.py
@@ -129,8 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# SendGrid Configuration
-
 
 # Login URL Configuration
 LOGIN_URL = '/login/'
